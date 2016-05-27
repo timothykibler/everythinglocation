a3284c2129f8279ec445b9985b17b13c01cddcb7
Timothy Kibler <tim.kibler@everythinglocation.com>
2016-05-27 10:53:20 -0700

    Fixed calling reference to undefined function
    Removed extra whitespace

--- everythinglocation.js
+++ everythinglocation.js
@@ -5,31 +5,31 @@ class EverythingLocation {
 	constructor(apikey) {
 		this.key = apikey || null;
 	}
-	
+
 	get key() {
 		return this._key
 	}
-	
+
 	set key(apikey) {
 		this._key = apikey || null;
 	}
-	
+
 	verify(query, callback) {
 		this.contactEL('address/verify', query, callback)
 	}
-	
+
 	complete(query, callback) {
 		this.contactEL('address/complete', query, callback)
 	}
-	
+
 	email(query, callback) {
-		contactEL('email/verify', query, callback)
+		this.contactEL('email/verify', query, callback)
 	}
-	
+
 	capture(query, callback) {
-		contactEL('address/capture', query, callback)
+		this.contactEL('address/capture', query, callback)
 	}
-	
+
 	contactEL(path, query, callback) {
 		request
 			.post('https://api.everythinglocation.com/' + path + '?' + this.key, {
